10bd12bd4236baf598383fd7ea56b0494ef97d8b
Tomás Gutiérrez <tomasosk@iantech.com.ar>
2025-08-07 15:58:39 -0300

    feat/tabs reportes

--- src/app/panel/reportes/page-client.tsx
+++ src/app/panel/reportes/page-client.tsx
@@ -20,6 +20,7 @@ import {
 } from "recharts";
 import { MultiSelect } from "~/components/multi-select";
 import dayjs from "dayjs";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
 
 export default function LockerOcupationPage() {
   const [tempEntidadesFiltro, setTempEntidadesFiltro] = useState<null | string[]>(null);
@@ -62,7 +63,12 @@ export default function LockerOcupationPage() {
     });
 
   const { data: capacityBySize } =
-    api.reports.getTotalBoxesAmountPerSize.useQuery();
+    api.reports.getTotalBoxesAmountPerSize.useQuery({
+      startDate,
+      endDate,
+      filterSerie: lockersSerie,
+      filterEntities: entidadesFiltro,
+    });
 
   const applyDateFilter = () => {
     setStartDate(tempStartDate);
@@ -251,210 +257,231 @@ export default function LockerOcupationPage() {
           <Button onClick={applyDateFilter}>Aplicar</Button>
         </div>
 
-        <table className="min-w-full border border-gray-300 bg-white">
-          <thead>
-            <tr>
-              <th className="border px-4 py-2">Día/Mes</th>
-              {sizesFiltrados
-                .map((size) => (
-                  <th key={size.id} className="border px-4 py-2">
-                    {size.nombre}
-                  </th>
-                ))
-              }
-              <th className="border px-4 py-2">Total</th>
-              <th className="border px-4 py-2">%</th>
-            </tr>
-          </thead>
-          <tbody>
-            {ocupationData.map((entry) => (
-              <tr key={entry.day}>
-                <td className="border px-4 py-2">{entry.day}</td>
-                {sizesFiltrados.map((size) => (
-                  <td key={size.id} className="border px-4 py-2">
-                    {entry.sizes[size.nombre!] || 0}
-                  </td>
-                ))}
-                <td className="border px-4 py-2">{entry.total}</td>
-                <td className="border px-4 py-2">
-                  {((entry.total / totalCapacity) * 100).toFixed(2)}%
-                </td>
-              </tr>
-            ))}
-            <tr>
-              <td className="border px-4 py-2 font-bold">Totales</td>
-              {sizesFiltrados.map((size) => (
-                <td key={size.id} className="border px-4 py-2 font-bold">
-                  {totalBySize[size.nombre!] || 0}
-                </td>
-              ))}
-              <td className="border px-4 py-2 font-bold">{grandTotal}</td>
-              <td className="border px-4 py-2 font-bold">100%</td>
-            </tr>
-            <tr>
-              <td className="border px-4 py-2 font-bold">Capacidad</td>
-              {sizesFiltrados.map((size) => (
-                <td key={size.id} className="border px-4 py-2 font-bold">
-                  {capacityBySize ? capacityBySize[size.nombre!] || 0 : 0}
-                </td>
-              ))}
-              <td className="border px-4 py-2 font-bold">{totalCapacity}</td>
-              <td className="border px-4 py-2 font-bold">-</td>
-            </tr>
-          </tbody>
-        </table>
-
-        <div className="mt-8 space-y-8">
-          <div style={{ width: "100%", height: 400 }}>
-            <Title>Porcentaje de Ocupación vs Días</Title>
-            <ResponsiveContainer className="py-8">
-              <BarChart
-                data={chartDataOccupation}
-                margin={{ top: 20, right: 30, left: 30, bottom: 20 }}
-              >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis
-                  dataKey="day"
-                  tick={{ fontSize: 12 }}
-                  label={{
-                    value: "Días",
-                    position: "insideBottom",
-                    offset: -10,
-                    fontSize: 14,
-                  }}
-                />
-                <YAxis
-                  domain={[0, 100]}
-                  tick={{ fontSize: 12 }}
-                  label={{
-                    value: "Porcentaje de Ocupación",
-                    angle: -90,
-                    position: "insideLeft",
-                    fontSize: 14,
-                  }}
-                />
-
-                <Tooltip />
-                <Legend wrapperStyle={{ fontSize: 12 }} />
-                <Bar
-                  dataKey="percentage"
-                  fill="#8884d8"
-                  name="Ocupación (%)"
-                  barSize={30}
-                />
-              </BarChart>
-            </ResponsiveContainer>
-          </div>
-          <div style={{ width: "100%", height: 400 }}>
-            <Title>Total de Ocupación</Title>
-            <ResponsiveContainer className="py-8">
-              <PieChart>
-                <Pie
-                  data={pieData}
-                  dataKey="value"
-                  nameKey="name"
-                  cx="50%"
-                  cy="50%"
-                  outerRadius={120}
-                  fill="#8884d8"
-                  label={({ name, percent }) =>
-                    `${name}: ${(percent * 100).toFixed(0)}%`
+        <Tabs defaultValue="tabla">
+          <TabsList className="grid w-full grid-cols-3">
+            <TabsTrigger value="tabla">Tabla</TabsTrigger>
+            <TabsTrigger value="povd">Porcentaje de Ocupación</TabsTrigger>
+            <TabsTrigger value="to">Total de Ocupación</TabsTrigger>
+          </TabsList>
+          <TabsList className="grid w-full grid-cols-3">
+            <TabsTrigger value="rpd">Reservas por día</TabsTrigger>
+            <TabsTrigger value="fpd">Facturación por día</TabsTrigger>
+            <TabsTrigger value="dpr">Duración Promedio</TabsTrigger>
+          </TabsList>
+          <TabsContent value="tabla">
+            <table className="min-w-full border border-gray-300 bg-white">
+              <thead>
+                <tr>
+                  <th className="border px-4 py-2">Día/Mes</th>
+                  {sizesFiltrados
+                    .map((size) => (
+                      <th key={size.id} className="border px-4 py-2">
+                        {size.nombre}
+                      </th>
+                    ))
                   }
-                >
-                  {pieData.map((_, index) => (
-                    <Cell
-                      key={`cell-${index}`}
-                      fill={COLORS[index % COLORS.length]}
-                    />
+                  <th className="border px-4 py-2">Total</th>
+                  <th className="border px-4 py-2">%</th>
+                </tr>
+              </thead>
+              <tbody>
+                {ocupationData.map((entry) => (
+                  <tr key={entry.day}>
+                    <td className="border px-4 py-2">{entry.day}</td>
+                    {sizesFiltrados.map((size) => (
+                      <td key={size.id} className="border px-4 py-2">
+                        {entry.sizes[size.nombre!] || 0}
+                      </td>
+                    ))}
+                    <td className="border px-4 py-2">{entry.total}</td>
+                    <td className="border px-4 py-2">
+                      {((entry.total / totalCapacity) * 100).toFixed(2)}%
+                    </td>
+                  </tr>
+                ))}
+                <tr>
+                  <td className="border px-4 py-2 font-bold">Totales</td>
+                  {sizesFiltrados.map((size) => (
+                    <td key={size.id} className="border px-4 py-2 font-bold">
+                      {totalBySize[size.nombre!] || 0}
+                    </td>
                   ))}
-                </Pie>
-                <Tooltip />
-                <Legend />
-              </PieChart>
-            </ResponsiveContainer>
-          </div>
-          <div style={{ width: "100%", height: 400 }}>
-            <Title>Reservas por Día</Title>
-            <ResponsiveContainer className="py-8">
-              <BarChart data={dailyReservationsData}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="day" />
-                <YAxis label={{ value: "Reservas", angle: -90 }} />
-                <Tooltip />
-                <Legend />
-                <Bar
-                  dataKey="reservations"
-                  fill="#ffc658"
-                  name="Reservas por Día"
-                />
-              </BarChart>
-            </ResponsiveContainer>
-          </div>
-          <div style={{ width: "100%", height: 400 }}>
-            <Title>Facturación por Día</Title>
-            <p>
-              <strong>Total Facturación:</strong> $
-              {billingByDay.total.toFixed(2)}
-            </p>
-            <ResponsiveContainer className="py-8">
-              <BarChart data={billingByDay.data}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="day" />
-                <YAxis label={{ value: "Facturación", angle: -90 }} />
-                <Tooltip />
-                <Legend />
-                <Bar dataKey="amount" fill="#82ca9d" name="Facturación" />
-              </BarChart>
-            </ResponsiveContainer>
-          </div>
-          <div style={{ width: "100%", height: 400 }}>
-            <Title>Duración Promedio de Reserva</Title>
-            <p className="mt-2 text-center text-gray-700">
-              <strong>Total de reservas:</strong>{" "}
-              {totalReservationsFromDuration}
-            </p>
-            <ResponsiveContainer className="py-8" width="100%" height={400}>
-              <BarChart
-                data={averageDurationData?.data}
-                margin={{
-                  top: 5,
-                  right: 30,
-                  left: 20,
-                  bottom: 5,
-                }}
-              >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis
-                  dataKey="days"
-                  label={{
-                    value: "Días",
-                    position: "insideBottom",
-                    offset: -5,
+                  <td className="border px-4 py-2 font-bold">{grandTotal}</td>
+                  <td className="border px-4 py-2 font-bold">100%</td>
+                </tr>
+                <tr>
+                  <td className="border px-4 py-2 font-bold">Capacidad</td>
+                  {sizesFiltrados.map((size) => (
+                    <td key={size.id} className="border px-4 py-2 font-bold">
+                      {capacityBySize ? capacityBySize[size.nombre!] || 0 : 0}
+                    </td>
+                  ))}
+                  <td className="border px-4 py-2 font-bold">{totalCapacity}</td>
+                  <td className="border px-4 py-2 font-bold">-</td>
+                </tr>
+              </tbody>
+            </table>
+          </TabsContent>
+          <TabsContent value="povd">
+            <div style={{ width: "100%", height: 400 }}>
+              <Title>Porcentaje de Ocupación vs Días</Title>
+              <ResponsiveContainer className="py-8">
+                <BarChart
+                  data={chartDataOccupation}
+                  margin={{ top: 20, right: 30, left: 30, bottom: 20 }}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis
+                    dataKey="day"
+                    tick={{ fontSize: 12 }}
+                    label={{
+                      value: "Días",
+                      position: "insideBottom",
+                      offset: -10,
+                      fontSize: 14,
+                    }}
+                  />
+                  <YAxis
+                    domain={[0, 100]}
+                    tick={{ fontSize: 12 }}
+                    label={{
+                      value: "Porcentaje de Ocupación",
+                      angle: -90,
+                      position: "insideLeft",
+                      fontSize: 14,
+                    }}
+                  />
+
+                  <Tooltip />
+                  <Legend wrapperStyle={{ fontSize: 12 }} />
+                  <Bar
+                    dataKey="percentage"
+                    fill="#8884d8"
+                    name="Ocupación (%)"
+                    barSize={30}
+                  />
+                </BarChart>
+              </ResponsiveContainer>
+            </div>
+          </TabsContent>
+          <TabsContent value="to">
+            <div style={{ width: "100%", height: 400 }}>
+              <Title>Total de Ocupación</Title>
+              <ResponsiveContainer className="py-8">
+                <PieChart>
+                  <Pie
+                    data={pieData}
+                    dataKey="value"
+                    nameKey="name"
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={120}
+                    fill="#8884d8"
+                    label={({ name, percent }) =>
+                      `${name}: ${(percent * 100).toFixed(0)}%`
+                    }
+                  >
+                    {pieData.map((_, index) => (
+                      <Cell
+                        key={`cell-${index}`}
+                        fill={COLORS[index % COLORS.length]}
+                      />
+                    ))}
+                  </Pie>
+                  <Tooltip />
+                  <Legend />
+                </PieChart>
+              </ResponsiveContainer>
+            </div>
+          </TabsContent>
+          <TabsContent value="rpd">
+            <div style={{ width: "100%", height: 400 }}>
+              <Title>Reservas por Día</Title>
+              <ResponsiveContainer className="py-8">
+                <BarChart data={dailyReservationsData}>
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="day" />
+                  <YAxis label={{ value: "Reservas", angle: -90 }} />
+                  <Tooltip />
+                  <Legend />
+                  <Bar
+                    dataKey="reservations"
+                    fill="#ffc658"
+                    name="Reservas por Día"
+                  />
+                </BarChart>
+              </ResponsiveContainer>
+            </div>
+          </TabsContent>
+          <TabsContent value="fpd">
+            <div style={{ width: "100%", height: 400 }}>
+              <Title>Facturación por Día</Title>
+              <p>
+                <strong>Total Facturación:</strong> $
+                {billingByDay.total.toFixed(2)}
+              </p>
+              <ResponsiveContainer className="py-8">
+                <BarChart data={billingByDay.data}>
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="day" />
+                  <YAxis label={{ value: "Facturación", angle: -90 }} />
+                  <Tooltip />
+                  <Legend />
+                  <Bar dataKey="amount" fill="#82ca9d" name="Facturación" />
+                </BarChart>
+              </ResponsiveContainer>
+            </div>
+          </TabsContent>
+          <TabsContent value="dpr">
+            <div style={{ width: "100%", height: 400 }}>
+              <Title>Duración Promedio de Reserva</Title>
+              <p className="mt-2 text-center text-gray-700">
+                <strong>Total de reservas:</strong>{" "}
+                {totalReservationsFromDuration}
+              </p>
+              <ResponsiveContainer className="py-8" width="100%" height={400}>
+                <BarChart
+                  data={averageDurationData?.data}
+                  margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
                   }}
-                />
-                <YAxis
-                  label={{
-                    value: "Reservas",
-                    angle: -90,
-                    position: "insideLeft",
-                  }}
-                />
-                <Tooltip />
-                <Legend />
-                <Bar dataKey="reservations" fill="#8884d8" name="Reservas" />
-                <Bar
-                  dataKey="averageDays"
-                  fill="#82ca9d"
-                  name="Promedio (días)"
-                />
-              </BarChart>
-            </ResponsiveContainer>
-            <p className="mt-4 text-center">
-              <strong>Promedio de duración:</strong>{" "}
-              {averageDurationData?.averageDuration.toFixed(2)} días
-            </p>
-          </div>
-        </div>
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis
+                    dataKey="days"
+                    label={{
+                      value: "Días",
+                      position: "insideBottom",
+                      offset: -5,
+                    }}
+                  />
+                  <YAxis
+                    label={{
+                      value: "Reservas",
+                      angle: -90,
+                      position: "insideLeft",
+                    }}
+                  />
+                  <Tooltip />
+                  <Legend />
+                  <Bar dataKey="reservations" fill="#8884d8" name="Reservas" />
+                  <Bar
+                    dataKey="averageDays"
+                    fill="#82ca9d"
+                    name="Promedio (días)"
+                  />
+                </BarChart>
+              </ResponsiveContainer>
+              <p className="mt-4 text-center">
+                <strong>Promedio de duración:</strong>{" "}
+                {averageDurationData?.averageDuration.toFixed(2)} días
+              </p>
+            </div>
+          </TabsContent>
+        </Tabs>
       </section>
     </LayoutContainer>
   );
